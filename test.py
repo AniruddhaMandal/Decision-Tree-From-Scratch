0d7bd08562ef3d8563cfe4eb57a42f21225f31f3
Aniruddha Mandal <ani96dh@gmail.com>
2020-08-05 20:20:08 +0530

    6th

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import pandas as pd 
 import count
-data = pd.read_csv("test.csv")
 
-print(count.best_axis_cut(data,"age","coolness"))
-
+data = pd.read_csv("churn.csv")
+print(count.best_axis_cut(data,"MonthlyCharges","Churn"))
