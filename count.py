8c1ef29a328150879eb92e02a74af2218dcb8cfd
Aniruddha Mandal <ani96dh@gmail.com>
2020-08-05 15:56:28 +0530

    5th

--- count.py
+++ count.py
@@ -11,24 +11,24 @@ def Gini(a,b):
 
 
 def best_axis_cut(dataset,column,target):
-    final_gini_low = 1000
-    final_gini_upp = 1000
+   
+    final_gini_low = 1
+    final_gini_upp = 1
     key_1 = dataset[target].unique()[0]
     key_2 = dataset[target].unique()[1]
-    #column_points = column_points[float(item) for item in dataset[column]]
+   
     for i in dataset[column]:
         low = dataset[dataset[column]<=i]
         upp = dataset[dataset[column]>i]
-        #print( "Uncool: ",Counter(low[target])['uncool'],'\n\n')
         gini_low = Gini(Counter(low[target])[key_1],Counter(low[target])[key_2])
         gini_upp = Gini(Counter(upp[target])[key_1],Counter(upp[target])[key_2])
-        print('\n',gini_low,gini_upp)
+        #print('\n',gini_low,gini_upp)
         if final_gini_low > gini_low:
             final_gini_low = gini_low
 
         if final_gini_upp > gini_upp:
             final_gini_upp = gini_upp
-        print(final_gini_low,final_gini_upp,'\n')
+        #print(final_gini_low,final_gini_upp,'\n')
     return final_gini_low,final_gini_upp    
 
         
